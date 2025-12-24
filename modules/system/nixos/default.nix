1fe7b90e995ecb02662920f34b6d7a024bb079f1
Pivatto <github@pivatto.dev>
2025-12-24 18:34:16 -0300

    feat: remove gnome bloatware

--- modules/system/nixos/default.nix
+++ modules/system/nixos/default.nix
@@ -27,8 +27,8 @@
     pulse.enable = true;
   };
 
-  # Printing
-  services.printing.enable = true;
+  # Printing (disabled - not using printer)
+  services.printing.enable = false;
 
   # System packages
   environment.systemPackages = with pkgs; [
@@ -39,8 +39,53 @@
     gnomeExtensions.blur-my-shell
   ];
 
-  # GNOME configuration
+  # GNOME configuration - Remove bloatware
   environment.gnome.excludePackages = with pkgs; [
-    # Remove unnecessary GNOME apps to keep system clean
+    # Media apps
+    cheese              # Webcam
+    epiphany            # Web browser
+    geary               # Email client
+    gnome-music         # Music player
+    totem               # Video player
+
+    # Utility apps
+    gnome-tour          # Welcome tour
+    gnome-contacts      # Contacts manager
+    gnome-maps          # Maps
+    gnome-weather       # Weather
+    gnome-clocks        # Clocks/timers
+    gnome-characters    # Character map
+    gnome-logs          # System logs viewer
+    gnome-font-viewer   # Font viewer
+    simple-scan         # Scanner
+    yelp                # Help viewer
+    evince              # PDF viewer (use your own)
+    gnome-calculator    # Calculator
+    snapshot            # Camera app
+    gnome-connections   # Remote desktop viewer
+    gnome-system-monitor # System monitor (use btop)
+    gnome-text-editor   # Text editor (use vim)
+    gnome-terminal      # Terminal (use kitty)
+    gnome-console       # Console (alternative terminal)
+    baobab              # Disk usage analyzer
+    gnome-software      # Software center (use nix)
+    seahorse            # Passwords and Keys (GnuPG/SSH key manager)
+
+    # Games
+    gnome-chess
+    gnome-mahjongg
+    gnome-mines
+    gnome-sudoku
+    gnome-tetravex
+    aisleriot           # Solitaire
+    atomix
+    five-or-more
+    four-in-a-row
+    hitori
+    iagno
+    lightsoff
+    quadrapassel
+    swell-foop
+    tali
   ];
 }
