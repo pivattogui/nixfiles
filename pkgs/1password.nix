a249272666ec81a2f3d32073484003d91db8aada
Pivatto <guilherme.pivatto@clinia.io>
2026-06-01 14:29:23 -0300

    fix(1password): migrate ssh matchBlocks to settings

--- pkgs/1password.nix
+++ pkgs/1password.nix
@@ -14,9 +14,9 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
+    settings = {
       "*" = {
-        identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+        IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
       };
     };
   };
